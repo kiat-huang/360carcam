d8a57bb0baf16e174283ebdb6c0ef90bb7d84908
Kiat Huang <kiat.huang@gmail.com>
2017-05-03 18:11:34 +0200

    smooth out analogue output from the RCWL-0516

---
+++ Arduino/RCWL-0516--smoothing/RCWL-0516--smoothing.ino
@@ -0,0 +1,62 @@
+/*
+
+  Smoothing
+
+  Intention:
+  1. Sense movement
+  
+
+
+  The circuit:
+      RCWL-0516 - http://www.ebay.fr/itm/172620108071
+      
+  Smoothing based on this code - https://goo.gl/94xwHh
+
+*/
+
+
+// Define the number of samples to keep track of.  The higher the number,
+// the more the readings will be smoothed, but the slower the output will
+// respond to the input.  Using a constant rather than a normal variable lets
+// use this value to determine the size of the readings array.
+const int numReadings = 20;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+int inputPin = A0;
+
+void setup() {
+  // initialize serial communication with computer:
+  Serial.begin(115200);
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+  total = numReadings * analogRead(inputPin);
+}
+
+void loop() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(inputPin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = abs(total / numReadings);
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  delay(200);        // delay in between reads for stability
+}
